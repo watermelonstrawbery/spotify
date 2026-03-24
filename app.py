c41c873ebeb88d59fe88c98e0c34b98c8117b195
watermelonstrawbery <sainas@kth.se>
2026-03-24 14:26:51 +0100

    Analyzing the data, looking for duplicates, empty cells...

---
+++ app.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.read_csv('dataset-spotify.csv')
+print(df.head())
+print(df.info())
+
+#Remove rows that contain empty cells
+new_df = df.dropna()
+
+print(new_df.info())
+
+print("Duplicates ")
+print(new_df.duplicated())
+
+if new_df.duplicated().sum() > 0:
+    print('There are duplicates')
+else:
+    print('There are no duplicates')
+
+
+
+
